d9511e50197fd9cfa273f79452895ad5fda7944c
Yincheng <974578444@qq.com>
2021-12-09 23:03:46 +0800

    早点写完早点睡

--- src/pages/City/index.jsx
+++ src/pages/City/index.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React,{ useState } from "react";
+import PubHeader from "../../components/PubHeader";
+import CurrentCity from "./CurrentCity";
+import CityList from "./CityList";
 
 const City = () => {
+
+  const [city,setCity] = useState("北京");
+
+  function onCityEvent(city) {
+    setCity(city)
+  }
+
   return(
     <div>
-      城市
+      <PubHeader title={"城市选择"}/>
+      <CurrentCity city={ city }/>
+      <CityList onEvent= { onCityEvent }/>
     </div>
   )
 }
